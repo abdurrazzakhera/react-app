75115b4ef4719c465af43dd695c66efbeb9f5465
abdurrazzakhera <hhera1413@gmail.com>
2022-04-01 21:40:04 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h2>WellCome To T-Shirts Mania</h2>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h3>The page Not Found !!!! 404</h3>
+    </div>
+  );
+};
+
+export default NotFound;
